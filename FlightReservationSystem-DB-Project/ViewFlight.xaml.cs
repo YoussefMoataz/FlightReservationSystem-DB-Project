d57e8b9eaa757427f9a56fec760caa9147c8fb84
hassan1876 <95700641+hassan1876@users.noreply.github.com>
2023-05-20 16:29:44 +0300

    added viewReservation
    added main functionality does not insert to data base

---
+++ FlightReservationSystem-DB-Project/ViewFlight.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightReservationSystem_DB_Project
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ViewFlight : Window
+    {
+        public ViewFlight()
+        {
+            InitializeComponent();
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\Documents\\FlightReservation.mdf;Integrated Security=True;Connect Timeout=30";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM FLIGHT WHERE FLIGHTID = @FLIGHTID;";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@FLIGHTID", 2);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    source.Text = reader["SOURCE"].ToString();
+                    destination.Text = reader["DESTINATION"].ToString();
+                    date.Text = reader["DEPARTUREDATE"].ToString();
+                    availableSeats.Text = reader["AVAILABLESEATS"].ToString();
+                }
+            }
+
+
+        }
+
+        private void Book_Click(object sender, RoutedEventArgs e)
+        {
+            if (economyChoice.IsChecked == true)
+            {
+                // Option 1 is selected
+                MessageBox.Show("economy is selected");
+            }
+            else if (buisnessChoice.IsChecked == true)
+            {
+                // Option 2 is selected
+                MessageBox.Show("buisness is selected");
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
